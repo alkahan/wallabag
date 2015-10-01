94f1ca6b9cba21e8da445a48a22a2b4732c09748
Nicolas Lœuillet <nicolas@loeuillet.org>
2015-10-01 16:28:38 +0200

    language selection on config screen

--- src/Wallabag/CoreBundle/DependencyInjection/WallabagCoreExtension.php
+++ src/Wallabag/CoreBundle/DependencyInjection/WallabagCoreExtension.php
@@ -2,6 +2,7 @@
 
 namespace Wallabag\CoreBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -11,6 +12,11 @@ class WallabagCoreExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
+        $processor = new Processor();
+        $configuration = new Configuration();
+        $config = $processor->processConfiguration($configuration, $configs);
+        $container->setParameter('wallabag_core.languages', $config['languages']);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }

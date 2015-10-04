5a5032745b165120294d1496620acf3c8f3f881a
Thomas Citharel <tcit@tcit.fr>
2015-10-04 19:08:54 +0200

    refactor

--- src/Wallabag/CoreBundle/Helper/EntriesExport.php
+++ src/Wallabag/CoreBundle/Helper/EntriesExport.php
@@ -5,54 +5,63 @@ namespace Wallabag\CoreBundle\Helper;
 use Wallabag\CoreBundle\Entity\Entry;
 use PHPePub\Core\EPub;
 use PHPePub\Core\Structure\OPF\DublinCore;
-use PHPePub\Core\Logger;
 use PHPZip\Zip\File\Zip;
 
-class Ebook
+class EntriesExport
 {
     private $format;
+    private $method;
     private $title;
     private $entries;
     private $authors = array('wallabag');
     private $language;
     private $tags;
-    private $generatedDate;
-    private $method;
 
-    public function __construct($entries, $format = 'epub', $method)
+    public function __construct($entries)
     {
         $this->entries = $entries;
-        $this->format = $format;
-        $this->method = $method;
-        $this->generatedDate = new \DateTime('now');
+
         foreach ($entries as $entry) {
             $this->tags[] = $entry->getTags();
         }
         if (count($entries) === 1) {
             $this->language = $entries[0]->getLanguage();
         }
+    }
+
+    /**
+     * Sets the category of which we want to get articles, or just one entry
+     *
+     * @param $method Method to get articles
+     */
+    public function setMethod($method)
+    {
+        $this->method = $method;
 
         switch ($this->method) {
-            case 'all':
+            case 'All':
                 $this->title = 'All Articles';
                 break;
-            case 'unread':
+            case 'Unread':
                 $this->title = 'Unread articles';
                 break;
-            case 'starred':
+            case 'Starred':
                 $this->title = 'Starred articles';
                 break;
-            case 'archive':
+            case 'Archive':
                 $this->title = 'Archived articles';
                 break;
             case 'entry':
                 $this->title = $this->entries[0]->getTitle();
                 break;
-
             default:
-                # code...
                 break;
         }
+    }
+
+    public function exportAs($format)
+    {
+        $this->format = $format;
 
         switch ($this->format) {
             case 'epub':
@@ -72,13 +81,15 @@ class Ebook
                 break;
 
             default:
-                # code...
                 break;
         }
     }
 
     private function produceEpub()
     {
+        /**
+         * Start and End of the book
+         */
         $content_start =
             "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
             ."<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:epub=\"http://www.idpf.org/2007/ops\">\n"
@@ -90,30 +101,32 @@ class Ebook
 
         $bookEnd = "</body>\n</html>\n";
 
-        $log = new Logger('wallabag', true);
-
         $book = new EPub(EPub::BOOK_VERSION_EPUB3);
-        $log->logLine('new EPub()');
-        $log->logLine('EPub class version: '.EPub::VERSION);
-        $log->logLine('Zip version: '.Zip::VERSION);
-        $log->logLine('getCurrentServerURL: '.$book->getCurrentServerURL());
-        $log->logLine('getCurrentPageURL..: '.$book->getCurrentPageURL());
+
+        /**
+         * Book metadata
+         */
 
         $book->setTitle($this->title);
         $book->setIdentifier($this->title, EPub::IDENTIFIER_URI); // Could also be the ISBN number, prefered for published books, or a UUID.
-            $book->setLanguage($this->language); // Not needed, but included for the example, Language is mandatory, but EPub defaults to "en". Use RFC3066 Language codes, such as "en", "da", "fr" etc.
-            $book->setDescription(_('Some articles saved on my wallabag'));
-        foreach ($this->authors as $author) {
+        $book->setLanguage($this->language); // Not needed, but included for the example, Language is mandatory, but EPub defaults to "en". Use RFC3066 Language codes, such as "en", "da", "fr" etc.
+        $book->setDescription(_('Some articles saved on my wallabag'));
+
+        foreach ($this->authors as $author)
+        {
             $book->setAuthor($author, $author);
         }
+
         $book->setPublisher('wallabag', 'wallabag'); // I hope this is a non existant address :)
-            $book->setDate(time()); // Strictly not needed as the book date defaults to time().
-            $book->setSourceURL("http://$_SERVER[HTTP_HOST]");
+        $book->setDate(time()); // Strictly not needed as the book date defaults to time().
+        $book->setSourceURL("http://$_SERVER[HTTP_HOST]");
 
         $book->addDublinCoreMetadata(DublinCore::CONTRIBUTOR, 'PHP');
         $book->addDublinCoreMetadata(DublinCore::CONTRIBUTOR, 'wallabag');
 
-        $log->logLine('Add Cover');
+        /**
+         * Front page
+         */
 
         $fullTitle = '<h1> '.$this->title."</h1>\n";
 
@@ -125,19 +138,20 @@ class Ebook
 
         $book->buildTOC();
 
+        /**
+         * Adding actual entries
+         */
+
         foreach ($this->entries as $entry) { //set tags as subjects
                 foreach ($this->tags as $tag) {
                     $book->setSubject($tag['value']);
                 }
 
-            $log->logLine('Set up parameters');
-
-            $chapter = $content_start.$entry->getContent().$bookEnd;
+            $chapter = $content_start . $entry->getContent() . $bookEnd;
             $book->addChapter($entry->getTitle(), htmlspecialchars($entry->getTitle()).'.html', $chapter, true, EPub::EXTERNAL_REF_ADD);
-            $log->logLine('Added chapter '.$entry->getTitle());
         }
         $book->finalize();
-        $zipData = $book->sendBook($this->title);
+        $book->sendBook($this->title);
     }
 
     private function produceMobi()
@@ -145,14 +159,25 @@ class Ebook
         $mobi = new \MOBI();
         $content = new \MOBIFile();
 
+        /**
+         * Book metadata
+         */
+
         $content->set('title', $this->title);
         $content->set('author', implode($this->authors));
         $content->set('subject', $this->title);
 
-        # introduction
+        /**
+         * Front page
+         */
+
         $content->appendParagraph('<div style="text-align:center;" ><p>'._('Produced by wallabag with PHPMobi').'</p><p>'._('Please open <a href="https://github.com/wallabag/wallabag/issues" >an issue</a> if you have trouble with the display of this E-Book on your device.').'</p></div>');
         $content->appendImage(imagecreatefrompng('themes/_global/img/appicon/apple-touch-icon-152.png'));
         $content->appendPageBreak();
+
+        /**
+         * Adding actual entries
+         */
 
         foreach ($this->entries as $entry) {
             $content->appendChapterTitle($entry->getTitle());
@@ -173,11 +198,19 @@ class Ebook
 
         $pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
+        /**
+         * Book metadata
+         */
+
         $pdf->SetCreator(PDF_CREATOR);
         $pdf->SetAuthor('wallabag');
         $pdf->SetTitle($this->title);
         $pdf->SetSubject('Articles via wallabag');
         $pdf->SetKeywords('wallabag');
+
+        /**
+         * Front page
+         */
 
         $pdf->AddPage();
         $intro = '<h1>' . $this->title . '</h1><div style="text-align:center;" >
@@ -188,10 +221,18 @@ class Ebook
 
         $pdf->writeHTMLCell(0, 0, '', '', $intro, 0, 1, 0, true, '', true);
 
-        foreach ($this->entries as $entry) {
-            foreach ($this->tags as $tag) {
+
+        /**
+         * Adding actual entries
+         */
+
+        foreach ($this->entries as $entry)
+        {
+            foreach ($this->tags as $tag)
+            {
                 $pdf->SetKeywords($tag['value']);
             }
+
             $pdf->AddPage();
             $html = '<h1>' . $entry->getTitle() . '</h1>';
             $html .= $entry->getContent();
